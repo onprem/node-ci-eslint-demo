9708978ebb4051e05778d4db23db5dccf479f9e9
Prem Saraswat <prmsrswt@gmail.com>
2021-01-11 21:19:34 +0530

    Setup eslint

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: [
+    'airbnb-base',
+  ],
+  env: {
+    commonjs: true,
+    es6: true,
+    node: true,
+  },
+};
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 
-const app = express()
+const app = express();
 
-app.get("/health", (req, res) => {
-  res.json({ status: 'UP' })
-})
+app.get('/health', (_req, res) => {
+  res.json({ status: 'UP' });
+});
 
 app.listen(5000, () => {
-  console.log(`Server running on http://localhost:5000`)
-})
+  // eslint-disable-next-line no-console
+  console.log('Server running on http://localhost:5000');
+});
